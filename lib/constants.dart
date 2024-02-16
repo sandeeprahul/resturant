f3d6c386d48dd9c07e72db3c39f5760bcec33c48
sandeeprahul <sandeeprahul156@gmail.com>
2024-02-16 11:33:23 +0530

    https

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,7 +3,7 @@
 class Constants{
 
   static const String apiHttpsUrl =
-      'http://employees.sanielsoft.com/api';
+      'https://employees.sanielsoft.com/api';
   /*static const String apiHttpsUrl =
       'https://employees.esolutionz.in/api';*/
 }
